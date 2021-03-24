a0cbdf3baf78544a0c3f8d1fb8785920813ff6fe
Mateusz Samek <samekmat@gmail.com>
2021-03-24 21:35:35 +0100

    Model changes(custom user) + get_foo_display in template index

--- oddam_app/models.py
+++ oddam_app/models.py
@@ -1,10 +1,58 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('Email musi być ustawiony'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
 
 TYPES = (
-    ('fundacja', 'fundacja'),
-    ('organizacja pozarządowa', 'organizacja pozarządowa'),
-    ('zbiórka lokalna', 'zbiórka lokalna'),
+    (1, 'fundacja'),
+    (2, 'organizacja pozarządowa'),
+    (3, 'zbiórka lokalna'),
 )
 
 
@@ -15,7 +63,7 @@ class Category(models.Model):
 class Institution(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField()
-    type = models.CharField(choices=TYPES, default='fundacja', max_length=128)
+    type = models.CharField(choices=TYPES, default=1, max_length=128)
     categories = models.ManyToManyField(Category)
 
     def __str__(self):
@@ -33,4 +81,4 @@ class Donation(models.Model):
     pick_up_date = models.DateField()
     pick_up_time = models.TimeField()
     pick_up_comment = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, default=None)
